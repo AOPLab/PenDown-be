b25ff1ae5cfafe72395b8eee14c400cc926b58eb
gary1030 <gary6658@ntu.im>
2022-04-11 14:23:03 +0800

    revise note model

--- src/model/Note.go
+++ src/model/Note.go
@@ -6,22 +6,23 @@ import (
 	"gorm.io/gorm"
 )
 
+// Note: filename should be "{create_time}_{random_string}"
 type Note struct {
 	gorm.Model
-	ID             int64     `gorm:"primary_key;auto_increment" json:"note_id"`
-	User_id        int64     `gorm:"not null" json:"user_id"`
-	Title          string    `gorm:"not null" json:"title"`
-	Description    string    `json:"description"`
-	Is_template    bool      `gorm:"default:false" json:"is_template"`
-	Course_id      int64     `json:"course_id"`
-	Price          int       `gorm:"default:0" json:"price"`
-	Pdf_Url        string    `gorm:"default:null" json:"pdf_url"`
-	Pdf_Preview    string    `gorm:"default:null" json:"pdf_preview_url"`
-	Goodnotes_Url  string    `gorm:"default:null" json:"goodnotes_url"`
-	Notability_Url string    `gorm:"default:null" json:"notability_url"`
-	CreatedAt      time.Time `json:"created_at"`
-	User           User      `gorm:"foreignKey:User_id;constraint:OnDelete:SET NULL;"`
-	Course         Course    `gorm:"foreignKey:Course_id;constraint:OnDelete:SET NULL;"`
+	ID                   int64     `gorm:"primary_key;auto_increment" json:"note_id"`
+	User_id              int64     `gorm:"not null" json:"user_id"`
+	Title                string    `gorm:"not null" json:"title"`
+	Description          string    `json:"description"`
+	Is_template          bool      `gorm:"default:false" json:"is_template"`
+	Course_id            int64     `json:"course_id"`
+	Price                int       `gorm:"default:0" json:"price"`
+	Pdf_filename         string    `gorm:"default:null" json:"pdf_filename"`
+	Pdf_preview_filename string    `gorm:"default:null" json:"pdf_preview_filename"`
+	Goodnotes_filename   string    `gorm:"default:null" json:"goodnotes_filename"`
+	Notability_filename  string    `gorm:"default:null" json:"notability_filename"`
+	CreatedAt            time.Time `json:"created_at"`
+	User                 User      `gorm:"foreignKey:User_id;constraint:OnDelete:SET NULL;"`
+	Course               Course    `gorm:"foreignKey:Course_id;constraint:OnDelete:SET NULL;"`
 }
 
 type Liked struct {
