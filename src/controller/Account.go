de496d9de4beceba6bef91925487ad619cb5aa7d
guanyiiiii-0225 <zoememy@gmail.com>
2022-04-19 00:43:19 +0800

    [unfinished] get public account

--- src/controller/Account.go
+++ src/controller/Account.go
@@ -10,10 +10,10 @@ import (
 )
 
 type EditAccountInput struct {
-	Username  string `json:"username"`
-	Full_name string `json:"full_name"`
-	Email     string `json:"email"`
-	Description  string `json:"description"`
+	Username    string `json:"username"`
+	Full_name   string `json:"full_name"`
+	Email       string `json:"email"`
+	Description string `json:"description"`
 }
 
 type EditPasswordInput struct {
@@ -21,12 +21,12 @@ type EditPasswordInput struct {
 	New_password string `json:"new_password" binding:"required"`
 }
 
-// GET /api/account/{account_id}/profile 
+// GET /api/account/{account_id}/profile
 // unfinished
 func GetPublicProfile(c *gin.Context) {
 	id := c.Params.ByName("account_id")
-    account_id, _ := strconv.ParseInt(id, 0, 64)
-	user, err := service.FindPublicProfile(account_id)
+	account_id, _ := strconv.ParseInt(id, 0, 64)
+	user, followers_num, following_num, note_num, err := service.FindPublicProfile(account_id)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -35,6 +35,12 @@ func GetPublicProfile(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"id": user.ID,
+		"username":      user.Username,
+		"description":   user.Description,
+		"status":        user.Status,
+		"bean":          user.Point,
+		"followers_num": followers_num,
+		"following_num": following_num,
+		"note_num":      note_num,
 	})
 }
--- src/service/Profile.go
+++ src/service/Profile.go
@@ -7,12 +7,18 @@ import (
 
 var PublicProfileFields = []string{"username", "description", "status", "bean"}
 
-func FindPublicProfile(account_id int64) (*model.User, error) {
+func FindPublicProfile(account_id int64) (*model.User, int64, int64, int64, error) {
 	user := &model.User{}
+	var followers_num int64
+	var following_num int64
+	var note_num int64
 	err := persistence.DB.Select(PublicProfileFields).Where("account_id=?", account_id).Find(&user).Error
 	if err != nil {
-		return nil, err
+		return nil, 0, 0, 0, err
 	} else {
-		return user, nil
+		persistence.DB.Model(&model.Follow{}).Where("Followee = ?", account_id).Count(&followers_num)
+		persistence.DB.Model(&model.Follow{}).Where("Follower = ?", account_id).Count(&following_num)
+		persistence.DB.Model(&model.Note{}).Where("User_ID = ?", account_id).Count(&note_num)
+		return user, followers_num, following_num, note_num, nil
 	}
 }
