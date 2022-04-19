6938f1488188e1028c4e9f084e1fce623ea023a2
gary1030 <gary6658@ntu.im>
2022-04-19 14:46:59 +0800

    db schema chenaged

--- src/model/Schoole.go
+++ src/model/Schoole.go
@@ -8,4 +8,5 @@ type School struct {
 	gorm.Model
 	ID          int64  `gorm:"primary_key;auto_increment" json:"school_id"`
 	School_name string `gorm:"not null" json:"school_name"`
+	View_cnt    int64  `gorm:"default:0" json:"view_cnt"`
 }
