bb425bf8d903eec74f3981091a0441a39780c79c
Shannon1923 <86880073+Shannon1923@users.noreply.github.com>
2022-05-02 11:37:47 +0800

    Update tag.go

--- src/service/tag.go
+++ src/service/tag.go
@@ -1,11 +1,8 @@
 package service
 
 import (
-	"errors"
-
 	"github.com/AOPLab/PenDown-be/src/model"
 	"github.com/AOPLab/PenDown-be/src/persistence"
-	"gorm.io/gorm"
 )
 
 func AddTag(tag_name string) (*model.Tag, error) {
@@ -14,22 +11,13 @@ func AddTag(tag_name string) (*model.Tag, error) {
 		Tag_name: tag_name,
 	}
 
-	var tag_exist model.Tag
-	err := persistence.DB.Where("Tag_name = ?", tag_name).First(&tag_exist).Error
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		db_err := persistence.DB.Model(&model.Tag{}).Create(&tag).Error
-		if db_err != nil {
-			return nil, db_err
-		}
-		return tag, nil
-	} else if err != nil {
-		return nil, err
-	} else {
-		return nil, errors.New("Tag already exists.")
+	db_err := persistence.DB.Model(&model.Tag{}).Where("Tag_name = ?", tag_name).FirstOrCreate(&tag).Error
+	if db_err != nil {
+		return nil, db_err
 	}
+	return tag, nil
 
 }
-
 func FindTags() ([]*model.Tag, error) {
 
 	var tags []*model.Tag
