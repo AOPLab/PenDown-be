38ff7533da1782310e02550ad78d2c97029f3847
gary1030 <gary6658@ntu.im>
2022-05-02 10:25:28 +0800

    search notes & template

--- src/controller/search.go
+++ src/controller/search.go
@@ -3,10 +3,23 @@ package controller
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/AOPLab/PenDown-be/src/service"
 	"github.com/gin-gonic/gin"
 )
+
+type LazyNoteOutput struct {
+	ID               int64     `json:"note_id"`
+	User_id          int64     `json:"user_id"`
+	Username         string    `json:"username"`
+	Title            string    `json:"title"`
+	View_cnt         int64     `json:"view_cnt"`
+	Saved_cnt        int64     `json:"saved_cnt"`
+	Note_type        string    `json:"note_type"`
+	Preview_filename string    `json:"preview_filename"`
+	CreatedAt        time.Time `json:"created_at"`
+}
 
 // q any string
 // note_type: all, notability, goodnotes
@@ -79,14 +92,89 @@ func Search(c *gin.Context) {
 		})
 		return
 	case "notes":
-		c.JSON(http.StatusOK, "Test")
+		result, total_cnt, err := service.SearchNote(q, offset, 12, note_type)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var note_outputs []*LazyNoteOutput
+		for _, note := range result {
+			note_output := &LazyNoteOutput{
+				ID:               note.ID,
+				User_id:          note.User_id,
+				Username:         note.Username,
+				Title:            note.Title,
+				Preview_filename: note.Preview_filename,
+				View_cnt:         note.View_cnt,
+				CreatedAt:        note.CreatedAt,
+			}
+			// add note type
+			if note.Notability_filename != "" && note.Goodnotes_filename != "" {
+				note_output.Note_type = "All"
+			} else if note.Notability_filename != "" {
+				note_output.Note_type = "Notability"
+			} else if note.Goodnotes_filename != "" {
+				note_output.Note_type = "Goodnotes"
+			}
+
+			// calculate saved cnt
+			cnt, save_err := service.GetNoteSavedCnt(note.ID)
+			if save_err != nil {
+				cnt = 0
+			}
+			note_output.Saved_cnt = cnt
+			note_outputs = append(note_outputs, note_output)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"notes":     note_outputs,
+			"total_cnt": total_cnt,
+		})
 		return
 	case "templates":
+		result, total_cnt, err := service.SearchTemplate(q, offset, 12, note_type)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var note_outputs []*LazyNoteOutput
+		for _, note := range result {
+			note_output := &LazyNoteOutput{
+				ID:               note.ID,
+				User_id:          note.User_id,
+				Username:         note.Username,
+				Title:            note.Title,
+				Preview_filename: note.Preview_filename,
+				View_cnt:         note.View_cnt,
+				CreatedAt:        note.CreatedAt,
+			}
+			// add note type
+			if note.Notability_filename != "" && note.Goodnotes_filename != "" {
+				note_output.Note_type = "All"
+			} else if note.Notability_filename != "" {
+				note_output.Note_type = "Notability"
+			} else if note.Goodnotes_filename != "" {
+				note_output.Note_type = "Goodnotes"
+			}
+
+			// calculate saved cnt
+			cnt, save_err := service.GetNoteSavedCnt(note.ID)
+			if save_err != nil {
+				cnt = 0
+			}
+			note_output.Saved_cnt = cnt
+			note_outputs = append(note_outputs, note_output)
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"q":         q,
-			"note_type": note_type,
-			"filter":    filter,
-			"offset":    offset,
+			"templates": result,
+			"total_cnt": total_cnt,
 		})
 		return
 	default:
