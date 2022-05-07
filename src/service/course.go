e7f9b71d70b9cbc66a6fc8abe20685fbc7d8624e
guanyiiiii-0225 <zoememy@gmail.com>
2022-05-07 19:33:58 +0800

    add school name and id

--- src/service/course.go
+++ src/service/course.go
@@ -27,14 +27,26 @@ type SearchCourseOutput struct {
 	ID          int64  `json:"course_id"`
 	Course_no   string `json:"course_no"`
 	Course_name string `json:"course_name"`
+	School_name string `json:"school_name"`
+	School_ID   int64  `json:"school_id"`
 }
 
-func SearchCourse(q string, offset int, limit int) ([]SearchCourseOutput, int64, error) {
-	var results []SearchCourseOutput
+func SearchCourse(q string, offset int, limit int) ([]*SearchCourseOutput, int64, error) {
+	var results []*SearchCourseOutput
 	var count int64
+	var schoolCourse []*model.Course
 	searchName := "%" + q + "%"
-	if err := persistence.DB.Limit(limit).Offset(offset).Table("courses").Select("ID, course_name, course_no").Where("course_name LIKE ?", searchName).Or("course_no LIKE ?", searchName).Find(&results).Error; err != nil {
+	if err := persistence.DB.Limit(limit).Offset(offset).Table("courses").Preload("School").Where("course_name LIKE ?", searchName).Or("course_no LIKE ?", searchName).Find(&schoolCourse).Error; err != nil {
 		return results, 0, err
+	}
+	for _, course := range schoolCourse {
+		var result SearchCourseOutput
+		result.ID = course.ID
+		result.Course_no = course.Course_no
+		result.Course_name = course.Course_name
+		result.School_ID = course.School_id
+		result.School_name = course.School.School_name
+		results = append(results, &result)
 	}
 	persistence.DB.Table("courses").Where("course_name LIKE ?", searchName).Or("course_no LIKE ?", searchName).Count(&count)
 	return results, count, nil
