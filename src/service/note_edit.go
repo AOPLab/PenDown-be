17e60cdc6cdb154d3d5bf261611549d84a0cf7c6
guanyiiiii-0225 <zoememy@gmail.com>
2022-05-11 11:52:56 +0800

    Update note_edit.go

--- src/service/note_edit.go
+++ src/service/note_edit.go
@@ -11,7 +11,7 @@ import (
 type EditNoteInput struct {
 	Title       string `json:"title" binding:"required"`
 	Description string `json:"description" binding:"required"`
-	Course_id   int64  `json:"course_id" binding:"required"`
+	Course_id   *int64 `json:"course_id"`
 	Bean        *int   `json:"bean" binding:"required"`
 	Is_template *bool  `json:"is_template" binding:"required"`
 }
@@ -25,13 +25,13 @@ func EditNote(user_id int64, note_id int64, form EditNoteInput) error {
 
 	courseIdBefore := note.Course_id
 	courseIdAfter := form.Course_id
-	if courseIdBefore != courseIdAfter {
+	if &courseIdBefore != courseIdAfter {
 		courseBefore := &model.Course{ID: courseIdBefore}
 		db_err := persistence.DB.Model(&courseBefore).Update("Note_cnt", gorm.Expr("Note_cnt - ?", 1)).Error
 		if db_err != nil {
 			return db_err
 		}
-		courseAfter := &model.Course{ID: courseIdAfter}
+		courseAfter := &model.Course{ID: *courseIdAfter}
 		db_err = persistence.DB.Model(&courseAfter).Update("Note_cnt", gorm.Expr("Note_cnt + ?", 1)).Update("Last_updated_time", time.Now()).Error
 		if db_err != nil {
 			return db_err
